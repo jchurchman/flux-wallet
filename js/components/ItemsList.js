d295b1d47649f4acbb12cb50ad3ec287279b8bf0
Joe Churchman <churchman.joe@gmail.com>
2017-11-09 11:04:23 -0800

    updated lifecycle methods in ItemsList, incl change listener

--- js/components/ItemsList.js
+++ js/components/ItemsList.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import WalletStore from '../stores/walletStore';
 
 export default class ItemsList extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            items: []
+            items: WalletStore.getAllItems()
         };
+        this._onChange = this._onChange.bind(this);
+    }
+
+    _onChange() {
+        this.setState({ items: WalletStore.getAllItems() });
+    }
+
+    componentWillMount() {
+        WalletStore.addChangeListener(this._onChange);
+    }
+
+    componentWillUnmount() {
+        WalletStore.removeChangeListener(this._onChange);
     }
 
     render() {
